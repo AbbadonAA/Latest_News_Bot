71a31ead08aa84d56c654e48d2a3634903f39ffa
Anton Pushkarev <pushkarevantona@gmail.com>
2023-04-12 23:36:40 +0300

    asyncio subprocess

--- app/api/endpoints/base_interact.py
+++ app/api/endpoints/base_interact.py
@@ -1,4 +1,4 @@
-import subprocess
+import asyncio
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,15 +15,27 @@ SPIDERS = ['rbc_spider', 'inosmi_spider']
 router = APIRouter()
 
 
+async def async_run(args, cwd=None):
+    """Асинхронный запуск подпроцессов, чтобы не блокировать приложение."""
+    process = await asyncio.create_subprocess_exec(*args, cwd=cwd)
+    await process.communicate()
+
+
 @router.post('/', dependencies=[Depends(current_superuser)])
 async def start_parsers(
     session: AsyncSession = Depends(get_session)
 ):
     """Запуск парсеров для заполнения БД."""
     count_before_add = await get_article_amount_from_db(session)
+    tasks = []
+    # Пауки запускаются в отдельных асинхронных подпроцессах.
     for spider in SPIDERS:
         args = ["python", "-m", "scrapy.cmdline", "crawl", spider]
-        subprocess.run(args, cwd="app/services/parse_news")
+        task = asyncio.ensure_future(
+            async_run(args, cwd='app/services/parse_news')
+        )
+        tasks.append(task)
+    await asyncio.gather(*tasks)
     count_after_add = await get_article_amount_from_db(session)
     num_added = count_after_add - count_before_add
     return {'result': f'В БД успешно добавлено {num_added} записей.'}
