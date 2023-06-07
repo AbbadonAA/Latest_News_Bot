459c472b1a396ad854bcf3cd82c14d9d8c21db80
Pushkarev Anton <pushkarevantona@gmail.com>
2023-06-07 23:31:27 +0300

    article 404 template fix for IV

--- app/api/endpoints/articles.py
+++ app/api/endpoints/articles.py
@@ -80,7 +80,7 @@ async def get_article_html(
         return templates.TemplateResponse(
             '404.html',
             {'request': request, **data},
-            status_code=HTTPStatus.NO_CONTENT,
+            status_code=HTTPStatus.OK,
         )
     infographic_links = [
         link.infographic_link for link in article.infographic_links
